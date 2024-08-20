f90b947688cd2cb8b179810db761acfb6a0c7539
APIVerve Team <hello@apiverve.com>
2024-08-20 10:45:07 -0500

    Bug Fixes

--- CitiesLookupAPIClient.cs
+++ CitiesLookupAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, citieslookupQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, CitiesLookupQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(citieslookupQueryOptions options = null)
+        public ResponseObj Execute(CitiesLookupQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(citieslookupQueryOptions options)
+        private string constructURL(CitiesLookupQueryOptions options)
         {
             string url = _apiEndpoint;
 
